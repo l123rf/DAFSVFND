4e683a5ab393af884c83a19c430a541cd1e89aab
l123rf <140942620+l123rf@users.noreply.github.com>
2025-02-28 15:38:45 +0800

    Update FeatureEncoder.py

--- model/FeatureEncoder.py
+++ model/FeatureEncoder.py
@@ -5,17 +5,12 @@ class TransformerEncoders(nn.Module):
     def __init__(self, layer_num, d_model, nhead):
         super(TransformerEncoders, self).__init__()
         
-        # 创建n个TransformerEncoderLayer
         self.layers = nn.ModuleList([
             nn.TransformerEncoderLayer(d_model=d_model, nhead=nhead, batch_first=True)
             for _ in range(layer_num)
         ])
         
-        # LayerNorm 防止梯度消失或爆炸
-        self.norm = nn.LayerNorm(d_model)
-    
     def forward(self, x):
-        # x的形状应该是 (batch_size, seq_len, d_model)
         for layer in self.layers:
             x = layer(x)  
         return x
